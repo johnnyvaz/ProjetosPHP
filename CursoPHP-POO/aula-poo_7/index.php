13b0356b65183bff8a6e7fbc57be8c5fb7ed1714
johnnyvaz <johnnyvaz@johnny-vaz>
2017-08-23 01:19:26 -0300

    iniciando

--- CursoPHP-POO/aula-poo_7/index.php
+++ CursoPHP-POO/aula-poo_7/index.php
@@ -12,10 +12,26 @@ and open the template in the editor.
     <body>
         <?php
         require_once 'Lutador.php';
+        $l = array();
+        $l[0] = new Lutador("Pretty Boy", "França" , 30 , 1.75 
+                , 90.9 , 11 , 2 , 1);
+        $l[1] = new Lutador("putscript", "Brasil" , 29 , 1.68 
+                , 57.9 , 14 , 2 , 1);
+        $l[2] = new Lutador("Dead Code", "EUA" , 35 , 1.65
+                , 80.9 , 12 , 2 , 1);
+        $l[3] = new Lutador("UFO Cobol", "Brasil" , 37 , 1.93 
+                , 81.9 , 13 , 2 , 1);
+        $l[4] = new Lutador("Snapshadow", "EUA" , 30 , 1.81
+                , 119.9 , 12 , 2 , 1);
         
-        $l = new Lutador("Pretty Boy", "França" , 30 , 1.75 , 68.9 , 11 , 2 , 1);
-        $l->status();
-        $l->ganharLuta();
+        $l[3]->apresentar();
+        $l[3]->status();
+        $l[3]->perderLuta();
+        $l[3]->ganharLuta();
+        $l[3]->status();
+        $l[3]->empatarLuta();
+        $l[3]->status();
+        $l[3]->apresentar();
         ?>
     </body>
 </html>
