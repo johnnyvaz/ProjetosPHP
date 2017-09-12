401d703cc78b88924d188d530a60f11331e6eeb5
Johnny Vaz <johnnyjohnnyjohnnyvaz@gmail.com>
2017-09-12 18:41:11 -0300

    aula 12

---
+++ CursoPHP-POO/aula_12/Mamifero.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'Animal.php';
+class Mamifero extends Animal{
+    private $corPelo;
+    public function alimentar() {
+        echo '<p>Mamando</p>';
+    }
+    public function emitirSom() {
+        echo '<p>Som do mamífero</p>';
+    }
+    public function locomover() {
+        echo '<p>Correndo</p>';
+    }
+    function getCorPelo() {
+        return $this->corPelo;
+    }
+
+    function setCorPelo($corPelo) {
+        $this->corPelo = $corPelo;
+    }
+
+
+}
---
+++ CursoPHP-POO/aula_12/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        require_once 'Mamifero.php';
+        require_once 'Reptil.php';
+        require_once 'Peixe.php';
+        require_once 'Ave.php';
+        
+        
+        
+        
+        ?>
+    </body>
+</html>
