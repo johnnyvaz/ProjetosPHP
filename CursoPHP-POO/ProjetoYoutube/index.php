1e74d0c4a14ae155442bdd5874d2dab350ca5b53
Johnny Vaz <johnnyjohnnyjohnnyvaz@gmail.com>
2017-10-18 12:19:16 -0200

    3niciando... 2

---
+++ CursoPHP-POO/ProjetoYoutube/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body><pre>
+        <?php
+        require_once 'AcoesVideo.php';
+        require_once 'Video.php';
+        
+        $v[0] = new Video("Aula 1 de POO");
+        $v[1] = new Video("Aula 12 de PHP");
+        $v[2] = new Video("Aula 15 de HTML5");
+        
+        
+        print_r($v);
+        
+        
+        
+        ?>
+</pre></body>
+</html>
